6ac0bdf199e785e7a1ed4cd9f0039e6629a0ef08
coolguy-codin-Rydham <rydhampreetsingh.gindra@gmail.com>
2024-04-06 21:43:00 +0530

    Almost their

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
+import Features from "./Components/Features";
 import Hero from "./Components/Hero";
 import HIghlights from "./Components/HIghlights";
 import Model from "./Components/Model";
 import Navbar from "./Components/Navbar";
-
+import * as Sentry from "@sentry/react"
 
 const App = () => {
+
   return (
   <main className="bg-black">
     <Navbar/>
     <Hero/>
     <HIghlights/>
     <Model/>
+    <Features/>
     
   </main>
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
